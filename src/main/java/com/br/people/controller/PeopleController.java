5857bd19d49a8925ca8b2f474d0cebfa6cfc6bae
emersonope <emersonope@gmail.com>
2023-04-09 23:00:48 -0300

    Exportar métricas do seu projeto para o formato do Prometheus utilizando o micrometer

--- src/main/java/com/br/people/controller/PeopleController.java
+++ src/main/java/com/br/people/controller/PeopleController.java
@@ -2,6 +2,7 @@ package com.br.people.controller;
 
 import com.br.people.model.PeopleModel;
 import com.br.people.service.PeopleService;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,21 +13,25 @@ public class PeopleController {
     @Autowired
     private PeopleService peopleService;
 
+    @Timed(value = "people.create", description = "Tempo de resposta do endpoint de criação de pessoas")
     @PostMapping
     public PeopleModel create(@RequestBody PeopleModel peopleModel) {
         return peopleService.create(peopleModel);
     }
 
+    @Timed(value = "people.findByCpf", description = "Tempo de resposta do endpoint de busca de pessoas por CPF")
     @GetMapping("/{cpf}")
     public PeopleModel findByCpf(@PathVariable String cpf) {
         return peopleService.findByCpf(cpf);
     }
 
+    @Timed(value = "people.update", description = "Tempo de resposta do endpoint de atualização de pessoas")
     @PutMapping("/{cpf}")
     public PeopleModel update(@PathVariable String cpf, @RequestBody PeopleModel peopleModel) {
         return peopleService.update(cpf, peopleModel);
     }
 
+    @Timed(value = "people.deleteByCpf", description = "Tempo de resposta do endpoint de exclusão de pessoas por CPF")
     @DeleteMapping("/{cpf}")
     public void deleteByCpf(@PathVariable String cpf) {
         peopleService.deleteByCpf(cpf);
